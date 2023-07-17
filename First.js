f9f4f12de621d969a1eb6a65e2bfe9b98f1fca4b
Made Agus Andi Gunawan <madeagusandi446@gmail.com>
2023-07-17 22:02:29 +0700

    test commit lagi

--- First.js
+++ First.js
@@ -6,8 +6,8 @@ http.createServer(function (req, res)
 {
   fs.readFile('index.html', function(err, data){
     res.writeHead(200, {'Content-Type': 'text/html'});
-    //var q = url.parse(req.url, true).query;
-    //var txt = q.year + " " + q.month;
+    var q = url.parse(req.url, true).query;
+    var txt = q.year + " " + q.month;
     res.write(data);
     return res.end();
   })
